10d3da7e12175d1021995ae4c85bfe54654ca810
Stoyan Tsekov <stoyan.tsekov@gmail.com>
2015-06-13 12:46:35 +0300

    first program

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aloha People :)");
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Pres any key to go to the Beach");
+            Console.ReadKey();    
+        }
+    }
+}
