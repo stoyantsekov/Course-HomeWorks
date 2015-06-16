f01f71bd6c0a36d0997ef48907bee3ed4f3dbf27
Stoyan Tsekov <stoyan.tsekov@gmail.com>
2015-06-16 15:21:13 +0300

    now it is ready :)

---
+++ MathHomeWork/Math4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number:  ");
+              string a = Console.ReadLine();
+              int aa = int.Parse(a);
+            Console.WriteLine("Pick a second number:  ");
+              string b = Console.ReadLine();
+              int bb = int.Parse(b);
+            Console.WriteLine(aa/bb);
+        }
+    }
+}
---
+++ MathHomeWork/Math5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is you first name? -     ");
+              string a = Console.ReadLine();
+            Console.WriteLine("What is you last name? -     ");
+              string b = Console.ReadLine();
+              string names = a + "_" + b;
+            Console.WriteLine(names);
+        }
+    }
+}
