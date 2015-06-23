4ce4db9b8d7ea5dcdbe853295a868c5e406c6074
Stoyan Tsekov <stoyan.tsekov@gmail.com>
2015-06-23 15:46:28 +0300

    I did not have the time to complete the homework,i will finish them as soon as possible. :(

---
+++ Loops/NumberN/Divideby7or11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divideby7or11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:  ");
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 0; i < x; i++)
+            {
+                if ((i % 7 == 0) || (i % 11 == 0))
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Loops/NumberN/LectureTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Not ready yet...
+            int first = Console.Read();
+            Console.WriteLine("Enter a number");
+            int counter = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a second number");
+            int seccounter = int.Parse(Console.ReadLine());
+            int result = counter + seccounter;
+
+            do
+            {
+
+                Console.Write(result);
+                Console.WriteLine();
+            }
+            while (true);
+        }
+    }
+}
